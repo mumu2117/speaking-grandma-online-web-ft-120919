03b94f39c09546b687d4011b9223a11f283fe1d6
Jennifer Miller <jmiller@archersystems.us>
2019-12-15 04:38:14 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -9,17 +9,12 @@
 
 # NO, NOT SINCE 1938!
 
- def speak_to_grandma(phrase)
-   if phrase == "I LOVE YOU GRANDMA!"
-     return "I LOVE YOU TOO PUMPKIN!"
-   end
-
-   if phrase == phrase.upcase
-     return "NO, NOT SINCE 1938!"
-
-    else 
-      "HUH?! SPEAK UP, SONNY!"
-    end
-
-
-end
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  end
+ end 
